132fafd2e87c13ff75bd20dc8e767f1cf734dd49
phi-line <phi.liney@gmail.com>
2018-02-03 18:03:20 -0800

    allowed cross origin requests

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,12 @@ var app = express();
 var server = app.listen(process.env.PORT || 8080, () => console.log('Server is running!'));
 var tt_key = process.env.TT_KEY;
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(express.static('public'));
 // app.use(bodyParser.json({ type: 'application/*+json' }))
 
@@ -43,44 +49,4 @@ function requestPickup(req, res){
 
 	res.status(200);
 	res.send(data.nodes);
-}
-
-app.get('/test', testAPI);
-function testAPI(req, res){
-	//https://<baseURL>/routing/<versionNumber>/matrix[/<outputFormat>]?key=<APIKEY>[&routingOption1=<routingValue1>&routingOption2=<routingValue2>...]
-	//https://api.tomtom.com/routing/1/matrix/xml?key=<APIKEY>&routeType=shortest&travelMode=truck
-	const options = {
-	method: 'POST',
-		uri: 'https://api.tomtom.com/routing/1/matrix/json?key=iKNkC5W8ARvRHaAbiVUE5kT3P45IGXtF&routeType=shortest&travelMode=truck',
-		body: {
-	      "origins": [
-	        {
-	            "point": {"latitude": 52.36006,"longitude": 4.85106}
-	        },
-	        {
-	            "point": {"latitude": 52.36187,"longitude": 4.85056}
-	        }
-	    	],
-
-		    "destinations": [
-		        {
-		            "point": {"latitude": 52.36006,"longitude": 4.85106}
-		        },
-		        {
-		            "point": {"latitude": 52.36187,"longitude": 4.85056}
-		        }
-		 		]
-		 },
-		json: true
-	}
-
-	request(options)
-	.then(function (response) {
-			console.log('meow');
-			res.send(response);
-	})
-	.catch(function (err) {
-			console.log('woof');
-			res.send(err)
-	})
 }
